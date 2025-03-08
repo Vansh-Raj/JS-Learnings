16c26bfba7996d73790bd1b4b56c389303e0d9a0
Vansh Rajvanshi <vanshraj50@gmail.com>
2025-03-08 17:14:25 +0530

    Backend created

--- Project -1/router/course.js
+++ Project -1/router/course.js
@@ -1,17 +1,24 @@
-
+const {userMiddleware} = require("../middleware/user")
 const {Router} = require("express")
-
+const {purchaseModel, courseModel} = require("../db")
 const courseRouter = Router();
 
-courseRouter.post("/purchase",function (req,res){
+courseRouter.post("/purchase",userMiddleware,async function (req,res){
+    const userId = req.userId;
+    const courseId = req.body.courseId;
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
     res.json({
-        message:"Signup endpoint"
+        message:"YOu have succesfully bought the course"
     })
 })
 
-courseRouter.get("/preview",function (req,res){
+courseRouter.get("/preview",async function (req,res){
+    const courses = await courseModel.find({});
     res.json({
-        message:"Signup endpoint"
+       courses
     })
 })
 
--- Project -1/router/user.js
+++ Project -1/router/user.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
-const {userModel} = require("../db");
+const {userModel, purchaseModel} = require("../db");
 const userRouter = Router();
+const {userMiddleware} = require("../middleware/user")
 const jwt = require("jsonwebtoken");
 const { JWT_USER_PASSWORD} = require("../config");
 
@@ -45,9 +46,13 @@ userRouter.post("/login",async function (req,res){
    
 })
 
-userRouter.get("/purchases",function (req,res){
+userRouter.get("/purchases",userMiddleware,async function (req,res){
+    const userId = req.userId;
+    const purchases = await purchaseModel.find({
+        userId
+    })
     res.json({
-        message:"Signup endpoint"
+        purchases
     })
 })
 
