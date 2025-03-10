c0dd20a35b2c1f0d1e7f08bba2def880c3587dec
Vansh Rajvanshi <vanshraj50@gmail.com>
2025-03-10 19:02:33 +0530

    React useState and compnent modularity

--- React/week-9/src/App.jsx
+++ React/week-9/src/App.jsx
@@ -1,79 +1,65 @@
-// Create a function component named App that will be rendered in the root element
+import {useState} from "react";
 function App() {
   // return JSX that will be rendered
   return (
       // Apply inline styles to the div element
       <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
-          <div
-              style={{
-                  display: "flex",
-                  justifyContent: "center",
-              }}
-          >
-              <div>
-                  <div>
-                      {/* Call PostComponent here with props to render it in the App component */}
-                      <PostComponent
-                          name={"Rohan"}
-                          subtitle={"20 followers"}
-                          time={"20m ago"}
-                          image={"https://imgs.search.brave.com/F5OoHgfmJlOgbB00VWzyvvcxuQZk7JOPrCYV51JMmNU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI3/Mzc5NDUyL3Bob3Rv/L2tpdHR5LmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz13UWcw/TXdTcXJxZkkzWHdN/WGREVjhkY2RZMlNH/WVh2YndPdE12cUxX/UWpJPQ"}
-                          description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
-                      />
-                  </div>
-                  <div>
-                      <div>
-                          {/* Call PostComponent here with props to render it in the App component */}
-                          <PostComponent
-                              name={"Harkirat"}
-                              subtitle={"Promoted"}
-                             
-                              image={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
-                              description={"How to get hired in 2024? I lost my Job in 2023, this is the roadmap I followed to get hired in 2024."}
-                          />
-                      </div>
-                  </div>
-
-              </div>
-          </div>
+       <ToggleMessage />
+       <ToggleMessage />
+       <ToggleMessage />
+         
       </div>
   );
 }
 
 // Create a style object to apply styles to the div element in PostComponent
-const style = {
-  width: 250,
-  backgroundColor: "white",
-  borderRadius: 10,
-  borderColor: "gray",
-  borderWidth: 1,
-  padding: 20,
-  margin: 10,
+// const style = {
+//   width: 250,
+//   backgroundColor: "white",
+//   borderRadius: 10,
+//   borderColor: "gray",
+//   borderWidth: 1,
+//   padding: 20,
+//   margin: 10,
+// };
+const ToggleMessage = () => {
+   const [notificationcount,setnotificationcount] = useState(0);
+
+   console.log("re-rendered")
+
+    function increment(){
+        setnotificationcount(notificationcount + 1);
+    }
+
+    return (
+        <div>
+            <button onClick={ increment}>
+                Toggle Message
+            </button>
+            {notificationcount}
+        </div>
+    );
 };
 
-// Create a function component named PostComponent with props to render it in the App component
-function PostComponent({ name, subtitle, time, image, description }) {
-  // return JSX that will be rendered
-  return (
-      // Apply style object to the div element
-      <div style={style}>
-          {/* display the name, subtitle, time, image, and description using props */}
-          <div style={{ display: "flex" }}>
-              <img src={image} style={{ width: 40, height: 40, borderRadius: 40 }} />
-              <div style={{ fontSize: 14, marginLeft: 10 }}>
-                  <b>{name}</b> 
-                  <div>{subtitle}</div>
-                  {time !== undefined && <div style = {{display:"flex",paddingTop:1}}>
-                  <div>{time}</div>
-                  <img src={"https://www.shutterstock.com/image-vector/clock-icon-trendy-flat-style-600nw-674379841.jpg"} style ={{width:12,height:12}}/>
-                  </div>}
-              </div>
-          </div>
+// function PostComponent({ name, subtitle, time, image, description }) {
+//   return (
+//       <div style={style}>
+//           <div style={{ display: "flex" }}>
+//               <img src={image} style={{ width: 40, height: 40, borderRadius: 40 }} />
+//               <div style={{ fontSize: 14, marginLeft: 10 }}>
+//                   <b>{name}</b> 
+//                   <div>{subtitle}</div>
+//                   {time !== undefined && <div style = {{display:"flex",paddingTop:1}}>
+//                   <div>{time}</div>
+//                   <img src={"https://www.shutterstock.com/image-vector/clock-icon-trendy-flat-style-600nw-674379841.jpg"} style ={{width:12,height:12}}/>
+//                   </div>}
+//               </div>
+//           </div>
 
-          <div style={{ fontSize: 14 }}>{description}</div>
-      </div>
-  );
-}
+//           <div style={{ fontSize: 14 }}>{description}</div>
+//       </div>
+//   );
+// }
 
-// Export App Component to use it in other components
-export default App;
+
+export default App; 
